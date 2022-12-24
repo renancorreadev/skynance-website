c2ef776843ff1976af02b28f66785e4917149560
renancorreadev <producer@skynance.net>
2022-12-24 00:27:46 -0300

    feat: initialize

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,109 @@
+import { Container, ToogleLanguage } from './styles'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { NavHashLink, HashLink } from 'react-router-hash-link'
+import {  useContext, useState } from 'react'
+import {GrLanguage} from 'react-icons/gr'
+import {MdInvertColors} from 'react-icons/md'
+import CV from '../../assets/renanCV.pdf'
+
+import "./translates.css"
+import {LanguageContext} from '../../context/language'
+
+
+export function Header() {
+  const [isActive, setActive] = useState(false)
+  // const [lang, setLanguage] = useState('PT')
+  const {language, setLanguage} = useContext(LanguageContext)
+
+  function toggleTheme() {
+    let html = document.getElementsByTagName('html')[0]
+    html.classList.toggle('light')
+  }
+
+  console.log(language)
+
+  function changeLanguage() {
+    let html = document.getElementsByTagName('html')[0]
+
+    if (html.getAttribute('lang') === 'en') {
+      html.setAttribute('lang', 'pt')
+      setLanguage('PT')
+    }else{
+      html.setAttribute('lang', 'en')
+      setLanguage('EN')
+    }
+  }
+
+  function closeMenu() {
+    setActive(false)
+  }
+
+  return (
+    <Container className="header-fixed">
+      <Router>
+        <HashLink smooth to="#home" className="logo">
+         <div className="header-image-container">
+            <div className="headerLogo"></div>
+         </div>
+        </HashLink>
+
+     
+  
+        <nav id="navbar" className={isActive ? 'active' : ''}>
+          <div  className="languageSet ">
+            <input
+              onChange={toggleTheme}
+              className="container_toggle"
+              type="checkbox"
+              id="switch"
+              name="mode"
+            />
+            <label htmlFor="switch">Toogle</label>
+            <MdInvertColors className="icon-Theme-color"/>
+          </div>
+
+          <div className="languageSet ">
+            <ToogleLanguage
+              onChange={changeLanguage}
+              className="container_toggle"
+              type="checkbox"
+              id="switchLang"
+              name="lang"
+            />
+             <label htmlFor="switchLang">Change Lang</label>
+            <span className="lang-title">{language}</span>
+            <GrLanguage className="icon-color"/>
+          
+           
+          </div>
+
+          <NavHashLink id="headerHome" smooth to="#home" onClick={closeMenu}>
+            Home
+          </NavHashLink>
+          <NavHashLink  id="header_about"smooth to="#about" onClick={closeMenu}>
+            About me
+          </NavHashLink>
+          <NavHashLink id="header_portfolio"smooth to="#portfolio" onClick={closeMenu}>
+            Portfolio
+          </NavHashLink>
+          <NavHashLink id="header_contact"smooth to="#contact" onClick={closeMenu}>
+            Contact
+          </NavHashLink>
+          <a href={CV} download className="button">
+            CV
+          </a>
+        </nav>
+
+        <div
+          aria-expanded={isActive ? 'true' : 'false'}
+          aria-haspopup="true"
+          aria-label={isActive ? 'Fechar menu' : 'Abrir menu'}
+          className={isActive ? 'menu active' : 'menu'}
+          onClick={() => {
+            setActive(!isActive)
+          }}
+        ></div>
+      </Router>
+    </Container>
+  )
+}
